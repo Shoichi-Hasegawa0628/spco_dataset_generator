1fb3291ce0c671d0044c449a9b15c363234ed94e
Shoichi Hasegawa <hasegawa.shoichi@em.ci.ritsumei.ac.jp>
2023-05-02 19:57:05 +0900

    added non word generator

--- src/spco_non_word_generator.py
+++ src/spco_non_word_generator.py
@@ -73,34 +73,81 @@ class SpCoNonWordGenerator():
     #
     #     return
 
-    # 追加学習
+    # 追加学習 # 孤立語バージョン
+    # def place_word_generator(self, num):
+    #     place_list = []
+    #     for i in range(num):
+    #         path = PLACE_WORD_DATA + str(i + 1)
+    #         if not os.path.exists(path):
+    #             os.makedirs(path)
+    #
+    #         if 0 < i + 1 <= 30:
+    #             place_list.append([place_names[0]])
+    #
+    #         elif 30 < i + 1 <= 60:
+    #             place_list.append([place_names[1]])
+    #
+    #         elif 60 < i + 1 <= 90:
+    #             place_list.append([place_names[2]])
+    #
+    #         elif 90 < i + 1 <= 120:
+    #             place_list.append([place_names[3]])
+    #
+    #         else:
+    #             word = []  # 物体を観測しない場合
+    #             place_list.append(word)
+    #             # CSVファイルに書き込む
+    #             with open(path + '/Otb.csv', 'w', newline='') as f:
+    #                 writer = csv.writer(f)
+    #                 for row in place_list:
+    #                     writer.writerow(row)
+    #
+    #
+    #     return
+
+    # 追加学習 # 文章バージョン
     def place_word_generator(self, num):
-        place_list = []
-        for i in range(num):
+        utterance_list = []
+        with open("/root/HSR/catkin_ws/src/spco_dataset_generator/data/output/test/sentence.csv", 'r') as f:
+            reader = csv.reader(f)
+            list = [row for row in reader]
+
+        for i in range(len(list)):
+            r = list[i][0].replace(',', '') # コンマを削除
+            s = r.replace('.', '') # ピリオドを削除
+            t = s.split()
+            utterance_list.append(t)
+
+
+
+        FilePath = "/root/HSR/catkin_ws/src/spco_dataset_generator/data/output/test/sentence_spco.csv"
+        with open(FilePath, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows(utterance_list)
+
+        print(utterance_list)
+        for i in range(len(utterance_list)):
             path = PLACE_WORD_DATA + str(i + 1)
             if not os.path.exists(path):
                 os.makedirs(path)
-
-            if 0 < i + 1 <= 30:
-                place_list.append([place_names[0]])
-
-            elif 30 < i + 1 <= 60:
-                place_list.append([place_names[1]])
-
-            elif 60 < i + 1 <= 90:
-                place_list.append([place_names[2]])
-
-            elif 90 < i + 1 <= 120:
-                place_list.append([place_names[3]])
-
-            else:
-                word = []  # 物体を観測しない場合
-                place_list.append(word)
-                # CSVファイルに書き込む
-                with open(path + '/Otb.csv', 'w', newline='') as f:
+            for j in range(i + 1):
+                with open(path + '/Otb.csv', 'a') as f:
                     writer = csv.writer(f)
-                    for row in place_list:
-                        writer.writerow(row)
+                    writer.writerow(utterance_list[j])
+
+        word = []  # 物体を観測しない場合
+        lists = len(utterance_list)
+        for i in range(num):
+            utterance_list.append(word)
+            path = PLACE_WORD_DATA + str(i + 1 + lists)
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+            # CSVファイルに書き込む
+            with open(path + '/Otb.csv', 'w', newline='') as f:
+                writer = csv.writer(f)
+                for row in utterance_list:
+                    writer.writerow(row)
 
 
         return
